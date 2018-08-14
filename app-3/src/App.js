fac3cc12f3628cb736f947706f3006b130d12d92
anthony62490 <anthonyjmagno@gmail.com>
2018-08-13 21:52:05 -0500

    Finished puzzle 3

--- app-3/src/App.js
+++ app-3/src/App.js
@@ -1,18 +1,39 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import SearchWidget from './components/SearchWidget'
 
-class App extends Component {
+class App extends Component
+{
+  constructor()
+  {
+    super();
+    this.state =
+    {
+      userInput: '',
+      clientList: ["Opal", "Kole", "Miller", "Beccaa", "Moss", "Singh", "Barbar", 
+      "Kylee", "Nola", "Joycelyn", "Lashawnda", "Trinh", "Kizzie", "Jacquline", 
+      "Grayce", "Augustus", "Erick", "Thelma", "Ma", "Gina", "Willis", "Lovella", 
+      "Becki", "Luella", "Francoise", "Shirlene", "Claude", "Guy", "Mechelle", 
+      "Odilia", "Kena", "Anissa", "Jaqueline", "Jacquelyn", "Elfriede", "Vanita", 
+      "Christin", "Franklyn", "Jerry", "Florentina", "Kieth", "Kris", "Eugene", 
+      "Alvaro", "Genia", "Lemuel", "Gena", "Karon", "Genoveva", "Clotilde", "Max", 
+      "Bryant", "Steve", "Vanetta", "Antonina", "Courtney", "Melody", "Paul"],
+    };
+  }
+
+  handleInput = (x) =>
+  {
+    this.setState({userInput: x.target.value});
+  }
+
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <SearchWidget 
+          handleInput={this.handleInput}
+          userInput={this.state.userInput}
+          clientList={this.state.clientList}
+        />
       </div>
     );
   }
