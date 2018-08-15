bd78a0a03d15c5fda80fed60360dc2e0e77c86b2
anthony62490 <anthonyjmagno@gmail.com>
2018-08-15 00:38:01 -0500

    Completed Puzzle 4

--- app-4/src/App.js
+++ app-4/src/App.js
@@ -2,17 +2,48 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
-  render() {
+class App extends Component 
+{
+  constructor()
+  {
+    super();
+    this.state = 
+    {
+      userName: "",
+      userPass: ""
+    };
+
+    this.handleInput =  this.handleInput.bind(this);
+    this.handleClick =  this.handleClick.bind(this);
+  }
+
+  handleInput(userin, isName)
+  {
+    console.log(userin);
+    switch(isName)
+    {
+      case 0:
+        this.setState({userName: userin.target.value});
+        break;
+      case 1:
+        this.setState({userPass: userin.target.value});
+        break;
+    }
+  }
+
+  handleClick()
+  {
+    const { userName, userPass } = this.state;
+    alert(`Username: ${userName} Password: ${userPass}`);
+  }
+
+  render() 
+  {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <input onChange={(e)=> this.handleInput(e, 0) } value={this.state.userName}/>
+        <input onChange={(e)=> this.handleInput(e, 1) } value={this.state.userPass}/>
+        <button onClick={this.handleClick}>Login</button>
       </div>
     );
   }
