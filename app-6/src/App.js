fbb976b1f003b89bbf1201858075c07e85722096
anthony62490 <anthonyjmagno@gmail.com>
2018-08-22 19:58:10 -0500

    finished puzzle 6

--- app-6/src/App.js
+++ app-6/src/App.js
@@ -1,20 +1,42 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Todo from './components/Todo'
 
-class App extends Component {
-  render() {
+class App extends Component 
+{
+  constructor()
+  {
+    super();
+    this.state =
+    {
+      taskList: [],
+      userInput: ''
+    }
+  }
+
+  buttonOnClickEvent()
+  {
+    //copy old array
+    let newArr = this.state.taskList.slice(0);
+    newArr.push(this.state.userInput);
+    this.setState({taskList:newArr, userInput:''});
+  }
+
+  render() 
+  {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>My to-do list:</h1>
+        <input onChange={(e) => this.setState({userInput: e.target.value})} value={this.state.userInput} />
+        <button onClick={() => this.buttonOnClickEvent() }>Add</button>
+        {
+          this.state.taskList.map( (e, i) =>
+            <Todo item={e} key={i} />
+          )
+        }
       </div>
-    );
+    ); //return
   }
 }
 
